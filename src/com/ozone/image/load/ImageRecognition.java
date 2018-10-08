55f4743285644538412b9a46781e97f9cffd5cba
Olivier Izad <olivier@twp-inc.com>
2018-10-08 16:13:39 -0400

    modifying image detection algorithm

--- src/com/ozone/image/load/ImageRecognition.java
+++ src/com/ozone/image/load/ImageRecognition.java
@@ -13,52 +13,109 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class ImageRecognition {
-
-	public static final int RESIZE = 50;
+ public class ImageRecognition {
 	
+	public static final int SIZE =100;
+	 
 	public static void main(String[] args) {
 		Date tic = new Date();
-		HashMap<Channel, int[][]> resized0 = resize(convertToMatrix(read(args[0])), RESIZE, RESIZE);
-		HashMap<Channel, int[][]> resized1 = resize(convertToMatrix(read(args[1])), RESIZE, RESIZE);
-//		display(convertToImage(blur(resized0)));
-//		display(convertToImage(blur(resized1)));
+		String image1 = (args == null || args.length < 2) ? "raw-people/tadas1.jpg" : args[0];
+		String image2 = (args == null || args.length < 2) ? "raw-people/tadas2.jpg" : args[1];
+//		String image1 = (args == null || args.length < 2) ? "raw-people/olivier1.jpg" : args[0];
+//		String image2 = (args == null || args.length < 2) ? "raw-people/olivier4.jpg" : args[1];
+//		String image1 = (args == null || args.length < 2) ? "raw-people/test.jpg" : args[0];
+//		String image2 = (args == null || args.length < 2) ? "raw-people/download1.jpeg" : args[1];
+//		String image2 = (args == null || args.length < 2) ? "raw-people/download2.jpeg" : args[1];
+		
+		int[][] original0 = convertToMatrix(read(image1));
+		int[][] original1 = convertToMatrix(read(image2));
+		
+		int[][] resized0 = resize(original0, SIZE, SIZE);
+		int[][] resized1 = resize(original1, SIZE, SIZE);
+				
+//		display(convertToImage(original0));
+//		display(convertToImage(original1));
+		
+//		int windowSize = SIZE/2;
+//		
+//		for(int i=0;i<windowSize;i++) {
+//			for(int j=0;j<windowSize;j++) 
+//				System.out.print(resized0[i][j] + "\t");
+//			System.out.println();
+//		}
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		for(int i=0;i<windowSize;i++) {
+//			for(int j=0;j<windowSize;j++) 
+//				System.out.print(resized1[i][j] + "\t");
+//			System.out.println();
+//		}
+		
+		int kernelSize = 100;
+		
+//		display(convertToImage(blur(resized0, kernelSize)));
+//		display(convertToImage(blur(resized1, kernelSize)));
 
-		double score = compareImages(blur(resized0), blur(resized1));
+		double score = compareImages(blur(resized0, kernelSize), blur(resized1, kernelSize));
 		System.out.println("score=" + String.format("%.2f", score) + "%");
 		Date toc = new Date();
 		double elapsed = (toc.getTime() - tic.getTime())/1000.0;
 		System.out.println("Ellapsed time: " + String.format("%.2f", elapsed)+ " seconds.");
 	}
 	
-	public static HashMap<Channel, int[][]> blur(HashMap<Channel, int[][]> matrix){
-		int kernelW = 2*(int)Math.round(Math.sqrt(RESIZE));
-		int kernelH = 2*(int)Math.round(Math.sqrt(RESIZE));
-		HashMap<Channel, int[][]> blurredImage = new HashMap<Channel, int[][]>();
-		for(Channel c : Channel.values())
-			blurredImage.put(c, new int[matrix.get(c).length-kernelW][matrix.get(c)[0].length-kernelH]);
+	public static int[][] gradient(int[][] m){
+		int[][] dmx = new int[m.length-1][m[0].length-1];
+		int[][] dmy = new int[m.length-1][m[0].length-1];
+		int[][] dm = new int[m.length-1][m[0].length-1];
 		
-		for(Channel c : Channel.values())
-			for(int i=0;i<matrix.get(c).length-kernelW;i++)
-				for(int j=0;j<matrix.get(c)[0].length-kernelH;j++){
-					int sum=0;
-					for(int k=0;k<kernelW;k++)
-						for(int l=0;l<kernelH;l++)
-							sum+=matrix.get(c)[i+k][j+l];
-					blurredImage.get(c)[i][j] = sum/(kernelW*kernelH);
-				}
+		for(int i=0;i<m.length-1;i++)
+			for(int j=0;j<m[0].length-1;j++) 
+				dmx[i][j] = m[i+1][j] - m[i][j];
+		for(int i=0;i<m.length-1;i++)
+			for(int j=0;j<m[0].length-1;j++)
+				dmy[i][j] = m[i][j+1] - m[i][j];
+		for(int i=0;i<m.length-1;i++)
+			for(int j=0;j<m[0].length-1;j++)
+				dm[i][j] = validate((int)Math.round(Math.sqrt(dmx[i][j]*dmx[i][j] + dmy[i][j]*dmy[i][j])));
+		return dm;
+	}
+	
+	public static int calculateAverage(int[][] m) {
+		int average = 0;
+		for(int i=0;i<m.length;i++) 
+			for(int j=0;j<m[0].length;j++) 
+				average += m[i][j]; 
+		average /= (m.length*m[0].length);
+		return average;
+	}
+	
+	public static int[][] blur(int[][] m, int kernelSize){
+		int kernelW = 2*(int)Math.round(Math.sqrt(kernelSize));
+		int kernelH = 2*(int)Math.round(Math.sqrt(kernelSize));
+		int[][] blurredImage = new int[m.length-kernelW][m[0].length-kernelH];
+		
+		for(int i=0;i<m.length-kernelW;i++)
+			for(int j=0;j<m[0].length-kernelH;j++){
+				int sum=0;
+				for(int k=0;k<kernelW;k++)
+					for(int l=0;l<kernelH;l++)
+						sum+=m[i+k][j+l];
+				blurredImage[i][j] = sum/(kernelW*kernelH);
+			}
 		return blurredImage;
 	}
 	
-	public static double compareImages(HashMap<Channel, int[][]> matrix1, HashMap<Channel, int[][]> matrix2){
+	public static double compareImages(int[][] matrix1, int[][] matrix2){
 		double sum = 0.0;
-		int w = matrix1.get(Channel.GREEN).length;
-		int h = matrix1.get(Channel.GREEN)[0].length;
-		for(Channel channel : Channel.values())
-			for(int i=0; i<w;i++)
-				for(int j=0;j<h;j++)
-					sum += Math.pow(matrix1.get(channel)[i][j] - matrix2.get(channel)[i][j], 2.0);
-		double variance = 100.0-Math.sqrt(sum/(3.0*h*w))/2.5;
+		int w = matrix1.length;
+		int h = matrix1[0].length;
+		
+		for(int i=0; i<w;i++)
+			for(int j=0;j<h;j++) 
+				sum += Math.pow(matrix1[i][j] - matrix2[i][j], 2.0);
+		double variance = 100.0-Math.sqrt(sum/(h*w))/2.55;
 		return variance;
 	}
 	
@@ -88,129 +145,69 @@ public class ImageRecognition {
 		BLUE,
 	}
 	
-	public static HashMap<Channel, int[][]> convertToMatrix(BufferedImage image){
+	public static int[][] convertToMatrix(BufferedImage image){
 		int width = image.getWidth();
 		int height = image.getHeight();
-		int [][] alpha = new int[height][width];
-		int[][] red = new int[height][width];
-		int[][] green = new int[height][width];
-		int[][] blue = new int[height][width];
+		int[][] matrix = new int[height][width];
 		
 		for(int i=0;i<height;i++){
 			for(int j=0;j<width;j++){
 				Color color = new Color(image.getRGB(j, i));
-				alpha[i][j] = color.getAlpha();
-				red[i][j] = color.getRed();
-				green[i][j] = color.getGreen();
-				blue[i][j] = color.getBlue();
+				matrix[i][j] = (color.getRed() + color.getGreen() + color.getBlue())/3;
 			}
 		}
-		
-		HashMap<Channel, int[][]> matrix = new HashMap<Channel, int[][]>();
-		
-		matrix.put(Channel.RED, red);
-		matrix.put(Channel.GREEN, green);
-		matrix.put(Channel.BLUE, blue);
-		
 		return matrix;
 	}
 	
-	public static BufferedImage convertToImage(HashMap<Channel, int[][]> matrix){
-		int height = matrix.get(Channel.RED).length;
-		int width = matrix.get(Channel.RED)[0].length;
-		int[][] red = matrix.get(Channel.RED);
-		int[][] green = matrix.get(Channel.GREEN);
-		int[][] blue = matrix.get(Channel.BLUE);
+	public static BufferedImage convertToImage(int[][] m){
+		int height = m.length;
+		int width = m[0].length;
 		
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		
-		for(int i=0;i<height;i++){
+		for(int i=0;i<height;i++)
 			for(int j=0;j<width;j++){
-				Color color = new Color(validate(red[i][j]), validate((int)(green[i][j])) , validate(blue[i][j]));
+				Color color = new Color(validate(m[i][j]), validate((int)(m[i][j])) , validate(m[i][j]));
 				image.setRGB(j, i, color.getRGB());
 			}
-		}
 		return image;
+	}
+	
+	public static int[][] threshold(int[][] m, int threshold){
+		int[][] o = new int[m.length][m[0].length];
+		for(int i=0;i<m.length;i++) 
+			for(int j=0;j<m[0].length;j++) 
+				o[i][j] = m[i][j] > threshold ? 255:0; 
+		return o;
 	}
 	
 	public static int validate(int value){
 		return value > 255 ? 255 : (value < 0 ? 0 : value);
 	}
-	
-//	public static int threshold(int value, double threshold){
-//		return value > threshold ? value : 0;
-//	}
-	
-//	public static HashMap<Channel, int[][]> deriveMatrix(HashMap<Channel, int[][]> matrix){
-//		HashMap<Channel, int[][]> derived = new HashMap<Channel, int[][]>();
-//		derived.put(Channel.RED, contrast(dydx(matrix.get(Channel.RED))));
-//		derived.put(Channel.GREEN, contrast(dydx(matrix.get(Channel.GREEN))));
-//		derived.put(Channel.BLUE, contrast(dydx(matrix.get(Channel.BLUE))));
-//		return derived;
-//	}
-	
-//	public static int[][] dydx(int[][] f){
-//		int height = f.length;
-//		int width = f[0].length;
-//		int[][] dydx = new int[height-2][width-2];
-//		
-//		for(int i=1;i<height-1;i++){
-//			for(int j=1;j<width-1;j++){
-//				dydx[i-1][j-1] = f[i+1][j+1] + f[i-1][j-1] - f[i-1][j+1] - f[i+1][j-1];
-//			}
-//		}
-//		
-//		return dydx;
-//	}
-	
-	public static HashMap<Channel, int[][]> resize(HashMap<Channel, int[][]> matrix, int w, int h){
-		HashMap<Channel, int[][]> resized = new HashMap<Channel, int[][]>();
-		for(Channel channel : Channel.values()){
-			int[][] pixels = new int[w][h]; 
-			for(int i=0;i<w;i++)
-				for(int j=0;j<h;j++)
-					pixels[i][j] = 0;
-			resized.put(channel, pixels);
-		}
-		for(Channel channel : Channel.values())
-			for(int i=0;i<matrix.get(channel).length;i++)
-				for(int j=0;j<matrix.get(channel)[0].length;j++){
-//					System.out.println(j + "\t" + h + "\t" + matrix.get(channel).length + "\t" + (int)Math.floor((j*h)/(0.0+matrix.get(channel)[0].length)));
-					resized.get(channel)[(int)Math.floor((i*w)/(0.0+matrix.get(channel).length))][(int)Math.floor((j*h)/(0.0+matrix.get(channel)[0].length))] += matrix.get(channel)[i][j];
-				}
 		
-		int numberOfPixelsInROI = (matrix.get(Channel.GREEN).length*matrix.get(Channel.GREEN)[0].length)/(w*h); 
+	public static int[][] resize(int[][] matrix, int w, int h){
+		int[][] resized = new int[w][h];
+		int[][] pixels = new int[w][h]; 
+		for(int i=0;i<w;i++)
+			for(int j=0;j<h;j++)
+				pixels[i][j] = 0;
 		
-		for(Channel channel : Channel.values())
-			for(int i=0;i<w;i++)
-				for(int j=0;j<h;j++)
-					resized.get(channel)[i][j] /= numberOfPixelsInROI;
+		HashMap<String, Integer> pixelCount = new HashMap<String, Integer>();
+		for(int i=0;i<w;i++) 
+			for(int j=0;j<h;j++) 
+				pixelCount.put(i+"-"+j, 0);
+		
+		for(int i=0;i<matrix.length;i++)
+			for(int j=0;j<matrix[0].length;j++){
+				int i2 = (i*w)/matrix.length;
+				int j2 = (j*h)/matrix[0].length;
+				resized[i2][j2] += matrix[i][j];
+				pixelCount.put(i2 + "-" + j2, pixelCount.get(i2 + "-" + j2)+1);
+			}
+		for(int i=0;i<w;i++)
+			for(int j=0;j<h;j++)
+				resized[i][j] /= pixelCount.get(i+"-"+j) == 0?1:pixelCount.get(i+"-"+j);
 		
 		return resized;
 	}
-	
-//	public static int[][] contrast(int[][] matrix){
-//		int height = matrix.length;
-//		int width = matrix[0].length;
-//		int[][] contrast = new int[height][width];
-//		
-//		int max = 0;
-//		int min = 255;
-//		
-//		for(int i=0;i<height;i++){
-//			for(int j=0;j<width;j++){
-//				max = Math.max(max, matrix[i][j]);
-//				min = Math.min(min, matrix[i][j]);
-//			}
-//		}
-//
-//		for(int i=0;i<height;i++){
-//			for(int j=0;j<width;j++){
-//				contrast[i][j] = (int)Math.round((255*(matrix[i][j]-min))/(double)(max-min));
-//			}
-//		}
-//		
-//		return contrast;
-//	}
-
 }
